00781aea6a98a607a6a139f3a25d4f40d7194fad
oscar <oscardiazmaldonado93@gmail.com>
2023-02-21 20:21:47 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_1/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Ejercicio1_1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_1
+{
+    public partial class MainPage : ContentPage
+    {
+        Operaciones operaciones;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            operaciones = new Operaciones();
+        }
+
+        private void btnsuma_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new Views.Resultado());
+
+            operaciones.Num1 = double.Parse(num1.Text);
+            operaciones.Num2 = double.Parse(num2.Text);
+
+            var resultado = operaciones.Suma();
+
+            Navigation.PushAsync(new Views.Resultado(resultado.ToString()));
+        }
+
+        private void btnresta_Clicked(object sender, EventArgs e)
+        {
+            operaciones.Num1 = double.Parse(num1.Text);
+            operaciones.Num2 = double.Parse(num2.Text);
+
+            var resultado = operaciones.Resta();
+
+            Navigation.PushAsync(new Views.Resultado(resultado.ToString()));
+        }
+
+        private void btndivi_Clicked(object sender, EventArgs e)
+        {
+
+            if (double.Parse(num2.Text) == 0)
+            {
+                var resultado = "No se puede dividir entre cero";
+
+                Navigation.PushAsync(new Views.Resultado(resultado));
+            }
+            else
+            {
+                operaciones.Num1 = double.Parse(num1.Text);
+                operaciones.Num2 = double.Parse(num2.Text);
+
+                var resultado = operaciones.Division();
+
+                Navigation.PushAsync(new Views.Resultado(resultado.ToString()));
+            }
+        }
+
+        private void btnmulti_Clicked(object sender, EventArgs e)
+        {
+            operaciones.Num1 = double.Parse(num1.Text);
+            operaciones.Num2 = double.Parse(num2.Text);
+
+            var resultado = operaciones.Multiplicacion();
+
+            Navigation.PushAsync(new Views.Resultado(resultado.ToString()));
+        }
+    }
+}
---
+++ Ejercicio1_1/Models/Operaciones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_1.Models
+{
+    internal class Operaciones
+    {
+        // Atributos 
+        private double num1;
+        private double num2;
+
+        // Propiedades 
+        public double Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+
+        public double Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+        // Metodos
+        public double Suma()
+        {
+            return num1 + num2;
+        }
+
+        public double Resta()
+        {
+            return num1 - num2;
+        }
+
+        public double Division()
+        {
+            return num1 / num2;
+        }
+
+        public double Multiplicacion()
+        {
+            return num1 * num2;
+        }
+    }
+}
